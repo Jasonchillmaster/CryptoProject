25f891ee42d8c1d2f49e525e2050e4f1f2b1cd9d
FinnDaDerp <144409215+FinnDaDerp@users.noreply.github.com>
2023-09-15 10:10:21 +0700

    Update THEBESTCODEINTHEWORLDCREATEDBYYOURSTRULYJASONMITCHELL.py
    added

--- THEBESTCODEINTHEWORLDCREATEDBYYOURSTRULYJASONMITCHELL.py
+++ THEBESTCODEINTHEWORLDCREATEDBYYOURSTRULYJASONMITCHELL.py
@@ -1,11 +1,14 @@
+# Import necessary modules
 import hashlib
 import json
 import random
 import time
 from datetime import datetime
 
+# Define a Blockchain class
 class Blockchain:
     def __init__(self):
+        # Initialize blockchain and current transactions
         self.chain = []
         self.current_transactions = []
 
@@ -13,6 +16,14 @@ class Blockchain:
         self.new_block(previous_hash="1", proof=100)
 
     def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in the blockchain.
+
+        :param proof: The proof of work
+        :param previous_hash: Hash of the previous block
+        :return: New block
+        """
+
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time.time(),
@@ -21,24 +32,45 @@ class Blockchain:
             'previous_hash': previous_hash or self.hash(self.chain[-1]) if self.chain else None,
         }
 
+        # Reset the current list of transactions
         self.current_transactions = []
+
+        # Append the block to the blockchain
         self.chain.append(block)
         return block
 
     @staticmethod
     def hash(block):
+        """
+        Create a SHA-256 hash of a block.
+
+        :param block: Block
+        :return: Hash
+        """
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
     def new_transaction(self, sender, recipient, amount):
+        """
+        Create a new transaction to go into the next mined block.
+
+        :param sender: Address of the sender
+        :param recipient: Address of the recipient
+        :param amount: Amount
+        :return: Index of the block that will hold this transaction
+        """
+
+        # Check if sender exists in wallet balances
         if sender not in wallet_balances:
             print(f"Sender {sender} not found in wallet balances. Transaction failed.")
             return -1
 
+        # Check if sender has enough balance for the transaction
         if wallet_balances[sender] < amount:
             print(f"Sender {sender} does not have enough balance to send {amount} ETH. Transaction failed.")
             return -1
 
+        # Add the transaction to the current list of transactions
         self.current_transactions.append({
             'sender': sender,
             'recipient': recipient,
@@ -49,8 +81,12 @@ class Blockchain:
 
     @property
     def last_block(self):
+        """
+        Get the last block in the blockchain.
+        """
         return self.chain[-1]
 
+# Define functions to save and load data
 def save_data():
     data = {
         "wallet_balances": wallet_balances,
@@ -67,15 +103,20 @@ def load_data():
     except FileNotFoundError:
         return {}, []
 
+# Create a list of Ethereal wallet addresses
 ethereal_wallets = [f"0xWallet{i}" for i in range(1, 6)]
+
+# Load wallet balances and transactions from a JSON file
 wallet_balances, transactions = load_data()
 
 # Initialize wallet balances with non-zero values
 for wallet in ethereal_wallets:
     wallet_balances.setdefault(wallet, round(random.uniform(1, 100), 2))
 
+# Create an instance of the Blockchain class
 blockchain = Blockchain()
 
+# Display a welcome message and user menu
 print("Welcome to Ethereal Blockchain Simulator!")
 while True:
     print("\nMenu:")
@@ -84,11 +125,12 @@ while True:
     print("3. Display Blockchain")
     print("4. Exit")
 
+    # Get the user's choice
     choice = input("Enter your choice (1/2/3/4): ")
 
     if choice == "1":
+        # Simulate Ethereal Transactions
         num_transactions = int(input("Enter the number of transactions you want to simulate: "))
-
         for _ in range(num_transactions):
             print("\nAvailable Wallets with Balances:")
             for i, wallet in enumerate(ethereal_wallets):
@@ -121,6 +163,7 @@ while True:
         print("Transactions simulated successfully!")
 
     elif choice == "2":
+        # Display Wallet Balances and Transactions
         print("\nWallet Balances:")
         for wallet, balance in wallet_balances.items():
             print(f"{wallet}: {balance} ETH")
@@ -131,6 +174,7 @@ while True:
             print(f"{i}. Sender: {sender}, Receiver: {receiver}, Amount: {amount} ETH")
 
     elif choice == "3":
+        # Display Blockchain
         print("\nBlockchain:")
         for block in blockchain.chain:
             print(f"Block {block['index']}:")
@@ -145,8 +189,10 @@ while True:
                 print(f"  - Amount: {transaction['amount']} ETH")
 
     elif choice == "4":
+        # Exit the program
         print("Thank you for using Ethereal Blockchain Simulator! Goodbye.")
         break
 
     else:
+        # Handle invalid choices
         print("Invalid choice. Please select a valid option (1/2/3/4).")
